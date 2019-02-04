00579850d6cad049e017f62a856caa7ea49c4da2
Halcy0nic <support@halcyonic.us>
2019-02-04 15:30:48 -0800

    Fixing install file

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo apt update && sudo apt upgrade
-sudo apt install hostapd dnsmasq
-sudo mkdir -p /var/www/SUBZero/ && sudo cp ./httpsServer.py /var/www/SUBZero && sudo cp wallpaper.jp /var/www/SUBZero &&  openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes && sudo cp ./server.pem /var/www/SUBZero
+sudo apt -y install hostapd dnsmasq
+sudo mkdir -p /var/www/SUBZero/ && sudo cp ./httpsServer.py /var/www/SUBZero && sudo cp wallpaper.jpg /var/www/SUBZero &&  openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes && sudo cp ./server.pem /var/www/SUBZero
  
 chmod +x ./subzero.sh && sudo cp ./subzero.sh /usr/local/bin
 (sudo crontab -l 2>/dev/null; echo "@reboot /usr/local/bin/subzero.sh") | crontab -
@@ -10,9 +10,9 @@ chmod +x ./subzero.sh && sudo cp ./subzero.sh /usr/local/bin
 sudo mv /etc/dhcpcd.conf /etc/dhcpcd.bak
 sudo mv /etc/dnsmasq.conf /etc/dnsmasq.bak
 sudo mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.bak
-sudo mv /etc/interfaces /etc/interfaces.bak
+sudo mv /etc/network/interfaces /etc/interfaces.bak
 
 sudo cp ./interfaces /etc/
 sudo cp ./dhcpcd.conf /etc/
 sudo cp ./dnsmasq.conf /etc/
-sudo cp ./hostapd /etc/hostapd/
+sudo cp ./hostapd.conf /etc/hostapd/
