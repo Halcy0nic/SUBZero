4a7db79bfd79db6c29325b26322bee24a82d0c30
Nick Engmann <cyengmann@gmail.com>
2019-03-21 23:40:16 -0700

    wrong order for install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-sudo systemctl unmask hostapd
-sudo systemctl enable hostapd
-sudo systemctl start hostapd
-
 sudo apt update && sudo apt upgrade
 sudo apt install -y hostapd dnsmasq
 sudo mkdir -p /var/www/SUBZero/ && sudo cp ./httpsServer.py /var/www/SUBZero && sudo cp wallpaper.jpg /var/www/SUBZero &&  openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes && sudo cp ./server.pem /var/www/SUBZero
@@ -20,5 +16,7 @@ sudo cp ./dhcpcd.conf /etc/
 sudo cp ./dnsmasq.conf /etc/
 sudo cp ./hostapd.conf /etc/hostapd/
 echo "DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"" | sudo tee -a /etc/default/hostapd
+sudo systemctl unmask hostapd
 sudo systemctl enable hostapd
+sudo systemctl start hostapd
 sudo systemctl enable dnsmasq
